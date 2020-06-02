037b6e0fc281ad201d3265de1d458a352a0d50a8
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-06-02 09:12:55 +0530

    Initial setup for API

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.get('/data', log, (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+}
